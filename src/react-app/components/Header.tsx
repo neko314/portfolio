1d7fe6886f5a09974cbc2c8d310fa23edb18a1d5
Keiko Kaneko <keiko.cda@gmail.com>
2026-01-02 16:57:24 +0900

    Set header link to top page

--- src/react-app/components/Header.tsx
+++ src/react-app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <h1 className="logo">neko314's Portfolio</h1>
+        <a href="/"><h1 className="logo">neko314's Portfolio</h1></a>
       </div>
     </header>
   );
